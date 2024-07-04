535ad3a509e59acd0487acffdc534e29f4229fdb
Renzo Garcia <renzo_garcia@live.com>
2024-07-04 11:43:57 +0800

    Updated project id details

--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+PROJECT_ID=$(gcloud config get-value project)
 
-export PROJECT_ID="account-pocs"
+export PROJECT_ID=$PROJECT_ID
 export BUCKET_FOR_UPLOAD="cymbal-search-poc"
 export ENGINE_1="cym-ds_1716228262297"
 export MODEL_1="gemini-pro"
